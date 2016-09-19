dc32d8c0fd5e141aac6c06e1a20df36d485b4b73
Joakim Nohlgård <joakim@nohlgard.se>
2016-09-19 11:25:37 +0200

    CMake: Add installation and packaging targets

---
+++ cmake/cpack-project-settings.cmake
@@ -0,0 +1,27 @@
+set(CPACK_PACKAGE_NAME "libarrowhead")
+set(CPACK_PACKAGE_VENDOR "Fotonic")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Arrowhead C++ interface")
+set(CPACK_PACKAGE_VERSION_MAJOR ${VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "CPack Component Example")
+
+# Available CPack component choices
+set(CPACK_COMPONENTS_ALL applications libraries headers)
+
+
+set(CPACK_COMPONENT_APPLICATIONS_DISPLAY_NAME "Tools and applications (ahq, coap-server)")
+set(CPACK_COMPONENT_LIBRARIES_DISPLAY_NAME "Shared libraries")
+set(CPACK_COMPONENT_HEADERS_DISPLAY_NAME "C++ Headers")
+# Let the C++ headers component depend on the library component
+set(CPACK_COMPONENT_HEADERS_DEPENDS libraries)
+# Let the applications component depend on the library component
+set(CPACK_COMPONENT_APPLICATIONS_DEPENDS libraries)
+
+set(CPACK_COMPONENT_APPLICATIONS_GROUP "Runtime")
+set(CPACK_COMPONENT_LIBRARIES_GROUP "Runtime")
+set(CPACK_COMPONENT_HEADERS_GROUP "Development")
+
+set(CPACK_COMPONENT_GROUP_DEVELOPMENT_DESCRIPTION
+   "Components for developers")
